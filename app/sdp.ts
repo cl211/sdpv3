497d2087fd1bb4475c820407e8c8ba423675df33
Hudmo168 <geoffroy.cloteaux@gadz.org>
2015-10-17 19:14:21 +0200

    (enh) Alignement de sdp.ts sur le modèle Mongoose

--- app/sdp.ts
+++ app/sdp.ts
@@ -1,14 +1,26 @@
 /** Les classes et interfaces utilisées dans le SDP */
 module sdp {
+  /** L'objet utilisateur */
   export interface user {
-    lastname?: string;
-    firstname?: string;
     buque?: string;
     fams?: string;
+    firstname?: string;
+    lastname?: string;
+    adress?: string;
+    latitude?: number;
+    longitude?: number;
+    phone?: string;
+    grouperegionals?: string;
+    boquettes?: string;
+    email1?: string;
+    email2?: string;
+    roles?: Array<string>;
   }
 }
 
+/** Module qui traduit ce qui sort de l'API */
 module api {
+  /** Les réponses de l'API */
   export interface answer {
     message: string;
     success: boolean;

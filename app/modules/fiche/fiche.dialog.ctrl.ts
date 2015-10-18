c62b07ed81bda1db981806c386790a69df7e3815
Florent Gros <florent.gros@gadz.org>
2015-10-18 02:11:02 +0200

    (add) Préparation des fiches utilisateur

---
+++ app/modules/fiche/fiche.dialog.ctrl.ts
@@ -0,0 +1,36 @@
+"use strict";
+/** Controller qui gère les fiches utilisateurs */
+class FicheDialogCtrl {
+  static IID: string = "FicheDialogCtrl";
+  static $inject: Array<string> = ["$mdDialog", "userData"];
+  /** Bouton pour fermer la popup sans sauvegarder */
+  hide: () => void;
+  /** Bouton pour fermer la popup en sauvegardant */
+  valider: () => void;
+  /** Les données de l'utilisateur courant */
+  user: sdp.user;
+
+  constructor($mdDialog: angular.material.IDialogService, userData: sdp.user) {
+    var vm: FicheDialogCtrl = this;
+    vm.hide = hide;
+    vm.valider = valider;
+    vm.user = userData;
+
+    function hide(): void {
+      $mdDialog.cancel();
+    }
+
+    function valider(): void {
+      var requete: any = prepareRequete(vm.user);
+      $mdDialog.hide(requete);
+    }
+
+    function prepareRequete(user: sdp.user):any {
+      var result: any = {};
+
+      return result;
+    }
+
+  }
+}
+angular.module("sdp").controller(FicheDialogCtrl.IID, FicheDialogCtrl);

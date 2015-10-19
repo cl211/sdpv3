735c3b1326d724b66882755615c3239f76bdece2
Florent Gros <florent.gros@gadz.org>
2015-10-19 20:38:29 +0200

    (del) fichiers js suprimmés

--- routes/api.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (api, models) {
-    require('./api.buquages')(api, models);
-    require('./api.enumerations')(api, models);
-    require('./api.users')(api, models);
-};
-//# sourceMappingURL=api.js.map
